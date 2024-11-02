fdf0e5b3d952b521704bb49066a21f140a98a34f
justclaner <justclaner@gmail.com>
2024-11-02 19:11:58 -0400

    Initial Commit

---
+++ Interactive/InteractiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveApp.swift
+//  Interactive
+//
+//  Created by Justin Zou on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
