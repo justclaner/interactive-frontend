2eb59824ad2097acfca276e803594d4442aab20d
justclaner <justclaner@gmail.com>
2024-11-06 21:55:27 -0500

    Login Page - updated UI - added basic NavigationStack logic

--- Interactive/InteractiveApp.swift
+++ Interactive/InteractiveApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct InteractiveApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
--- Interactive/LoginView.swift
+++ Interactive/LoginView.swift
@@ -11,120 +11,141 @@ struct LoginView: View {
     @State private var isLogging: Bool = false
     @State private var username: String = ""
     @State private var password: String = ""
+    @State private var path : [Int] = []
     var body: some View {
-        ZStack {
-            AngularGradient(colors: [
-                //Color.red, Color.blue
-                Helper.hexColor(hexCode: "#AF945F").opacity(0.3),
-                Helper.hexColor(hexCode: "#735E46").opacity(0.3),
-                Helper.hexColor(hexCode: "#AD7644").opacity(0.3),
-                Helper.hexColor(hexCode: "#FFF3B6").opacity(0.3)
-            ], center: .center, angle: .zero)
-            .blur(radius:100)
-            .rotationEffect(.degrees(155))
-            .frame(width:676,height:453)
-            .offset(x:0,y:-320)
-                
-            AngularGradient(colors: [
-                Helper.hexColor(hexCode: "#AF945F").opacity(0.3),
-                Helper.hexColor(hexCode: "#735E46").opacity(0.3),
-                Helper.hexColor(hexCode: "#AD7644").opacity(0.3),
-                Helper.hexColor(hexCode: "#FFF3B6").opacity(0.3)
-            ], center: .center, angle: .zero)
-            .blur(radius:100)
-            .frame(width:755,height:720)
-            .rotationEffect(.degrees(-70))
-            .offset(x:100,y:200)
-            
-            Rectangle()
-            .fill(Helper.hexColor(hexCode: "#3C3B3B").opacity(0.65))
-            .blur(radius:100)
-            .frame(width:281,height:224)
-            .offset(x:120,y:-380)
+        NavigationStack(path:$path) {
         
-            Rectangle()
-            .fill(Helper.hexColor(hexCode: "#3C3B3B").opacity(0.65))
-            .blur(radius:100)
-            .frame(width:281,height:224)
-            .offset(x:-200,y:350)
+            ZStack {
+//            AngularGradient(colors: [
+//                //Color.red, Color.blue
+//                Helper.hexColor(hexCode: "#AF945F").opacity(0.3),
+//                Helper.hexColor(hexCode: "#735E46").opacity(0.3),
+//                Helper.hexColor(hexCode: "#AD7644").opacity(0.3),
+//                Helper.hexColor(hexCode: "#FFF3B6").opacity(0.3)
+//            ], center: .center, angle: .zero)
+//            .blur(radius:100)
+//            .rotationEffect(.degrees(155))
+//            .frame(width:676,height:453)
+//            .offset(x:0,y:-320)
 
-            Color.white.opacity(0)
+            Color.white.opacity(0.001)
                 .ignoresSafeArea()
                 .onTapGesture() {
                     if (isLogging) {
                         isLogging.toggle()
                     }
                 }
-            VStack {
-                Spacer()
-                Text("i")
-                    .font(.system(size:128,weight:.bold))
-                    .frame(width:296,height:155)
-                    //.position(x:(49 + 296/2),y:(289 + 155/2))
-                    .padding([.bottom],-5)
+                .background(
+                    Image("Background1")
+                        .resizable()
+                        .ignoresSafeArea()
+                )
+            
                 VStack {
-                    if (!isLogging) {
+                    Spacer()
+                    VStack{
+                        Circle()
+                            .fill(Helper.hexColor(hexCode: "#FFDD1A"))
+                            .frame(width:16,height:16)
+                            .padding([.bottom],-4)
+                        Rectangle()
+                            .fill(Helper.hexColor(hexCode: "#FFDD1A"))
+                            .frame(width:16,height:52)
+                            .clipShape(RoundedRectangle(cornerRadius:12.11))
+                            .padding([.bottom],10)
+                        
+                    }
+                    .background(
+                     Circle()
+                        .fill(Helper.hexColor(hexCode:"#333333"))
+                        .frame(width:113,height:113)
+                    )
+                    .offset(x:0,y:-40)
+                    VStack {
+                            Button(action: {
+                                path = [1,2,3]
+                                isLogging.toggle()
+                            }) {
+                                Text("login")
+                                    .font(.system(size:17,weight:.bold))
+                                    .tint(Color.black.opacity(0.75))
+                                    .padding()
+                                    .frame(maxWidth:.infinity,maxHeight:.infinity)
+                            }
+                            
+                            .frame(width:253,height:41)
+                            .background(Helper.hexColor(hexCode: "#FFDD1A"))
+                            .clipShape(RoundedRectangle(cornerRadius:20))
+                            .overlay(RoundedRectangle(cornerRadius: 20)
+                                .stroke(.white.opacity(0.6), lineWidth: 1)
+                            )
+                            .padding([.vertical],10)
+                            
+                        
+//                        if (!isLogging) {
+//                            Button(action: {
+//                                isLogging.toggle()
+//                            }) {
+//                                Text("login")
+//                                    .font(.system(size:17,weight:.bold))
+//                                    .tint(Color.black.opacity(0.75))
+//                                    .padding(10)
+//                            }
+//                            .frame(width:253,height:41)
+//                            .background(Helper.hexColor(hexCode: "#FFDD1A"))
+//                            .clipShape(RoundedRectangle(cornerRadius:20))
+//                            .overlay(RoundedRectangle(cornerRadius: 20)
+//                                .stroke(.white.opacity(0.6), lineWidth: 1)
+//                            )
+//                            .padding([.vertical],10)
+//                        } else {
+//                            TextField("username/email",text:$username)
+//                                .font(.system(size:16,weight:.regular))
+//                                .tint(Color.black.opacity(0.75))
+//                                .padding([.horizontal],20)
+//                                .padding([.vertical],10)
+//                                .frame(width:253,height:39)
+//                                .background(Color.black.opacity(0.05))
+//                                .clipShape(RoundedRectangle(cornerRadius:20))
+//                                .overlay(RoundedRectangle(cornerRadius:20)
+//                                    .stroke(.white.opacity(0.6), lineWidth:1)
+//                                )
+//                                .padding([.bottom],10)
+//                            TextField("password",text:$password)
+//                                .font(.system(size:16,weight:.regular))
+//                                .tint(Color.black.opacity(0.75))
+//                                .padding([.horizontal],20)
+//                                .padding([.vertical],10)
+//                                .frame(width:253,height:39)
+//                                .background(Color.black.opacity(0.05))
+//                                .clipShape(RoundedRectangle(cornerRadius:20))
+//                                .overlay(RoundedRectangle(cornerRadius:20)
+//                                    .stroke(.white.opacity(0.6), lineWidth:1)
+//                                )
+//                                .padding([.bottom],10)
+//                        }
                         Button(action: {
-                            isLogging.toggle()
+                            //action
                         }) {
-                            Text("create account")
+                            Text("create an account")
                                 .font(.system(size:17,weight:.bold))
                                 .tint(Color.black.opacity(0.75))
                                 .padding(10)
                         }
                         .frame(width:253,height:41)
-                        .background(Color.black.opacity(0.17))
+                        .background(Helper.hexColor(hexCode: "#FFDD1A"))
                         .clipShape(RoundedRectangle(cornerRadius:20))
                         .overlay(RoundedRectangle(cornerRadius: 20)
                             .stroke(.white.opacity(0.6), lineWidth: 1)
                         )
-                        .padding([.vertical],10)
-                    } else {
-                        TextField("username/email",text:$username)
-                            .font(.system(size:16,weight:.regular))
-                            .tint(Color.black.opacity(0.75))
-                            .padding([.horizontal],20)
-                            .padding([.vertical],10)
-                            .frame(width:253,height:39)
-                            .background(Color.black.opacity(0.05))
-                            .clipShape(RoundedRectangle(cornerRadius:20))
-                            .overlay(RoundedRectangle(cornerRadius:20)
-                                .stroke(.white.opacity(0.6), lineWidth:1)
-                            )
-                            .padding([.bottom],10)
-                        TextField("password",text:$password)
-                            .font(.system(size:16,weight:.regular))
-                            .tint(Color.black.opacity(0.75))
-                            .padding([.horizontal],20)
-                            .padding([.vertical],10)
-                            .frame(width:253,height:39)
-                            .background(Color.black.opacity(0.05))
-                            .clipShape(RoundedRectangle(cornerRadius:20))
-                            .overlay(RoundedRectangle(cornerRadius:20)
-                                .stroke(.white.opacity(0.6), lineWidth:1)
-                            )
-                            .padding([.bottom],10)
                     }
-                    Button(action: {
-                        //action
-                    }) {
-                        Text("log in")
-                            .font(.system(size:17,weight:.bold))
-                            .tint(Color.black.opacity(0.75))
-                            .padding(10)
-                    }
-                    .frame(width:253,height:41)
-                    .background(Color.black.opacity(0.19))
-                    .clipShape(RoundedRectangle(cornerRadius:20))
-                    .overlay(RoundedRectangle(cornerRadius: 20)
-                        .stroke(.white.opacity(0.6), lineWidth: 1)
-                    )
+                    Spacer()
                 }
-                Spacer()
+                .offset(x:0,y:-60)
             }
-            .offset(x:0,y:-60)
-            
-            
+            .navigationDestination(for: Int.self) {
+                pageNum in Text("You selected \(pageNum)")
+            }
         }
     }
 }
