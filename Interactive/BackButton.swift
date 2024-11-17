fff076c0e23fb64235b40700f6451d9c6383e2ad
justclaner <justclaner@gmail.com>
2024-11-17 14:39:41 -0500

    Created Back Button and Large Logo Views for better organization

---
+++ Interactive/BackButton.swift
@@ -0,0 +1,18 @@
+//
+//  BackButton.swift
+//  Interactive
+//
+//  Created by Justin Zou on 11/17/24.
+//
+
+import SwiftUI
+
+struct BackButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackButton()
+}
---
+++ Interactive/LargeLogo.swift
@@ -0,0 +1,33 @@
+//
+//  Logo.swift
+//  Interactive
+//
+//  Created by Justin Zou on 11/17/24.
+//
+
+import SwiftUI
+
+struct LargeLogo: View {
+    var body: some View {
+        VStack{
+            Ellipse()
+                .fill(Helper.hexColor(hexCode: "#FFDD1A"))
+                .frame(width:16,height:14)
+                .padding([.bottom],-4)
+            Rectangle()
+                .fill(Helper.hexColor(hexCode: "#FFDD1A"))
+                .frame(width:16,height:52)
+                .clipShape(RoundedRectangle(cornerRadius:12.11))
+                .padding([.bottom],10)
+        }
+            .background(
+                Circle()
+                    .fill(Helper.hexColor(hexCode:"#333333"))
+                    .frame(width:113,height:113)
+            )
+    }
+}
+
+#Preview {
+    LargeLogo()
+}
