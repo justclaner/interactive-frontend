ce012aa43e7b5a3b9c90c37f89d7a3db207fc46c
justclaner <justclaner@gmail.com>
2024-11-29 19:14:50 -0500

    Added all social media app logos to assets.

---
+++ Interactive/Views/NetworksPage.swift
@@ -0,0 +1,19 @@
+//
+//  NetworksPage.swift
+//  Interactive
+//
+//  Created by Justin Zou on 11/29/24.
+//
+// Interests filter
+
+import SwiftUI
+
+struct NetworksPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetworksPage()
+}
