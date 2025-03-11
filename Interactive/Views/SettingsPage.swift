d8b08fe8512269ccc0162a841ba4175f7cb884ab
justclaner <justclaner@gmail.com>
2025-03-11 13:49:59 -0400

    added privacy and policy to settings page and logging out

--- Interactive/Views/SettingsPage.swift
+++ Interactive/Views/SettingsPage.swift
@@ -15,6 +15,9 @@ struct SettingsPage: View {
     
     @State var email: String = UserDefaults.standard.string(forKey: "email") ?? ""
     @FocusState var emailFocus: Bool
+    
+    var data = UserData()
+    
     var body: some View {
         ZStack {
             Color.white.opacity(0.001)
@@ -34,16 +37,17 @@ struct SettingsPage: View {
                     .font(.system(size:Control.mediumFontSize, weight:.semibold))
                     .foregroundStyle(Color.white)
                     .frame(width: Control.maxWidth, alignment: .center)
-                    .padding([.bottom], Control.largeFontSize)
-                Text("Account Settings")
-                    .font(.system(size:Control.smallFontSize, weight:.semibold))
-                    .foregroundStyle(Color.white)
-                    .frame(width: Control.maxWidth, alignment: .leading)
+            ScrollView {
                 VStack {
-                    TextField("", text: $phoneNumber, prompt: Text("Phone Number")
-                        .font(.system(size:Control.tinyFontSize,weight:.semibold))
-                        .foregroundColor(Control.hexColor(hexCode: "#CCCCCC").opacity(0.6))
-                    )
+                    Text("Account Settings")
+                        .font(.system(size:Control.smallFontSize, weight:.semibold))
+                        .foregroundStyle(Color.white)
+                        .frame(width: Control.maxWidth, alignment: .leading)
+                    VStack {
+                        TextField("", text: $phoneNumber, prompt: Text("Phone Number")
+                            .font(.system(size:Control.tinyFontSize,weight:.semibold))
+                            .foregroundColor(Control.hexColor(hexCode: "#CCCCCC").opacity(0.6))
+                        )
                         .font(.system(size:Control.tinyFontSize,weight:.semibold))
                         .foregroundColor(Control.hexColor(hexCode: "#CCCCCC"))
                         .padding(Control.tinyFontSize)
@@ -57,11 +61,11 @@ struct SettingsPage: View {
                         .textInputAutocapitalization(.never)
                         .focused($phoneNumberFocus)
                         .padding([.bottom], Control.getScreenSize().height * 0.005)
-                    
-                    TextField("", text: $email, prompt: Text("Email")
-                        .font(.system(size:Control.tinyFontSize,weight:.semibold))
-                        .foregroundColor(Control.hexColor(hexCode: "#CCCCCC").opacity(0.6))
-                    )
+                        
+                        TextField("", text: $email, prompt: Text("Email")
+                            .font(.system(size:Control.tinyFontSize,weight:.semibold))
+                            .foregroundColor(Control.hexColor(hexCode: "#CCCCCC").opacity(0.6))
+                        )
                         .font(.system(size:Control.tinyFontSize,weight:.semibold))
                         .foregroundColor(Control.hexColor(hexCode: "#CCCCCC"))
                         .padding(Control.tinyFontSize)
@@ -75,12 +79,106 @@ struct SettingsPage: View {
                         .textInputAutocapitalization(.never)
                         .focused($emailFocus)
                         .padding([.bottom], Control.getScreenSize().height * 0.005)
+                    }
+                    Text("Privacy and Safety")
+                        .font(.system(size:Control.smallFontSize, weight:.semibold))
+                        .foregroundStyle(Color.white)
+                        .frame(width: Control.maxWidth, alignment: .leading)
+                        .padding([.top], Control.getScreenSize().height * 0.02)
+                    VStack {
+                        Button(action: {
+                            //go to account privacy page
+                        }) {
+                            Text("Account privacy")
+                                .font(.system(size:Control.tinyFontSize,weight:.semibold))
+                                .foregroundColor(Control.hexColor(hexCode: "#CCCCCC"))
+                                .padding([.trailing, .leading], Control.tinyFontSize)
+                                .frame(width:Control.maxWidth,height: Control.mediumHeight, alignment: .leading)
+                                .background(Control.hexColor(hexCode: "#4D4D4D"))
+                                .clipShape(RoundedRectangle(cornerRadius:8))
+                                .overlay(RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Control.hexColor(hexCode: "#4D4D4D"), lineWidth: 1)
+                                )
+                                .padding([.bottom], Control.getScreenSize().height * 0.005)
+                        }
+                        
+                        Button(action: {
+                            //go to safety guidelines page
+                        }) {
+                            Text("Safety Guidelines")
+                                .font(.system(size:Control.tinyFontSize,weight:.semibold))
+                                .foregroundColor(Control.hexColor(hexCode: "#CCCCCC"))
+                                .padding([.trailing, .leading], Control.tinyFontSize)
+                                .frame(width:Control.maxWidth,height: Control.mediumHeight, alignment: .leading)
+                                .background(Control.hexColor(hexCode: "#4D4D4D"))
+                                .clipShape(RoundedRectangle(cornerRadius:8))
+                                .overlay(RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Control.hexColor(hexCode: "#4D4D4D"), lineWidth: 1)
+                                )
+                                .padding([.bottom], Control.getScreenSize().height * 0.005)
+                        }
+                        
+                        Button(action: {
+                            //go to account privacy page
+                        }) {
+                            Text("Account privacy")
+                                .font(.system(size:Control.tinyFontSize,weight:.semibold))
+                                .foregroundColor(Control.hexColor(hexCode: "#CCCCCC"))
+                                .padding([.trailing, .leading], Control.tinyFontSize)
+                                .frame(width:Control.maxWidth,height: Control.mediumHeight, alignment: .leading)
+                                .background(Control.hexColor(hexCode: "#4D4D4D"))
+                                .clipShape(RoundedRectangle(cornerRadius:8))
+                                .overlay(RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Control.hexColor(hexCode: "#4D4D4D"), lineWidth: 1)
+                                )
+                                .padding([.bottom], Control.getScreenSize().height * 0.005)
+                        }
+                        Spacer()
+                        
+                    }
+                    
+                    //logging out and deleting account
+                    VStack {
+                        Button(action: {
+                            data.cleanCompleteStorage()
+                            path = ["Login"]
+                        }) {
+                            Text("Log out")
+                                .font(.system(size:Control.tinyFontSize,weight:.semibold))
+                                .foregroundColor(Control.hexColor(hexCode: "#CCCCCC"))
+                                .padding([.trailing, .leading], Control.tinyFontSize)
+                                .frame(width:Control.maxWidth,height: Control.mediumHeight, alignment: .leading)
+                                .background(Control.hexColor(hexCode: "#4D4D4D"))
+                                .clipShape(RoundedRectangle(cornerRadius:8))
+                                .overlay(RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Control.hexColor(hexCode: "#4D4D4D"), lineWidth: 1)
+                                )
+                                .padding([.bottom], Control.getScreenSize().height * 0.005)
+                        }
+                        
+                        Button(action: {
+                            //delete account
+                        }) {
+                            Text("Delete account")
+                                .font(.system(size:Control.tinyFontSize,weight:.bold))
+                                .foregroundColor(Color.red)
+                                .padding([.trailing, .leading], Control.tinyFontSize)
+                                .frame(width:Control.maxWidth,height: Control.mediumHeight, alignment: .leading)
+                                .background(Control.hexColor(hexCode: "#4D4D4D"))
+                                .clipShape(RoundedRectangle(cornerRadius:8))
+                                .overlay(RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Control.hexColor(hexCode: "#4D4D4D"), lineWidth: 1)
+                                )
+                                .padding([.bottom], Control.getScreenSize().height * 0.005)
+                        }
+                        Spacer()
+                    }
+                    .padding([.top], Control.largeFontSize * 2)
+                    Spacer()
                 }
-                Text("Privacy and Safety")
-                    .font(.system(size:Control.smallFontSize, weight:.semibold))
-                    .foregroundStyle(Color.white)
-                    .frame(width: Control.maxWidth, alignment: .leading)
-                    .padding([.top], Control.getScreenSize().height * 0.02)
+                .border(Color.red, width: 2)
+            }   //end of scrollview
+                
                 Spacer()
             }
         }
